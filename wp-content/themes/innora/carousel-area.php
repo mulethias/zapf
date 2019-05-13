31d2991415472d27308386b58c8efe62a6d21dad
Rabson <rabsoninnora@gmail.com>
2019-05-13 21:31:58 +0200

    updated front page

--- wp-content/themes/innora/carousel-area.php
+++ wp-content/themes/innora/carousel-area.php
@@ -12,9 +12,13 @@
                 <!-- Content -->
                 <div class="room-content">
                     <h2 data-animation="fadeInUp" data-delay="100ms"><?php echo $preview_room['room_one_preview_title']?></h2>
-                    <h3 data-animation="fadeInUp" data-delay="300ms"><?php echo $preview_room['price']?><span>/ Day</span></h3>
+                    <h3 data-animation="fadeInUp" data-delay="300ms"><?php echo $preview_room['price']?><span></span></h3>
                     <ul class="room-feature" data-animation="fadeInUp" data-delay="500ms">
-                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['room_preview_message_one']?></span> <span></span></li> 
+                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['premium_room_detail_one']?></span> <span></span></li> 
+                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['premium_room_detail_two']?></span> <span></span></li> 
+                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['premium_room_detail_three']?></span> <span></span></li> 
+                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['premium_room_detail_four']?></span> <span></span></li> 
+                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['premium_room_detail_five']?></span> <span></span></li> 
                     </ul>
                     <a href="#" class="btn roberto-btn mt-30" data-animation="fadeInUp" data-delay="700ms">View Details</a>
                 </div>
@@ -28,9 +32,14 @@
                 <!-- Content -->
                 <div class="room-content">
                     <h2 data-animation="fadeInUp" data-delay="100ms"><?php echo $preview_room['room_one_preview_title_two']?></h2>
-                    <h3 data-animation="fadeInUp" data-delay="300ms"><?php echo $preview_room['price_two']?><span>/ Day</span></h3>
+                    <h3 data-animation="fadeInUp" data-delay="300ms"><?php echo $preview_room['price_two']?><span></span></h3>
                     <ul class="room-feature" data-animation="fadeInUp" data-delay="500ms">
-                        <li><span><i class="fa fa-check"></i><?php echo $preview_room['room_preview_message_two']?></span> <span></span></li>
+                    <li><span><i class="fa fa-check"></i> <?php echo $preview_room['room_detail_one']?></span> <span></span></li> 
+                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['room_detail_two']?></span> <span></span></li> 
+                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['room_detail_three']?></span> <span></span></li> 
+                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['room_detail_four']?></span> <span></span></li> 
+                        <li><span><i class="fa fa-check"></i> <?php echo $preview_room['room_detail_five']?></span> <span></span></li> 
+                       
                     </ul>
                     <a href="#" class="btn roberto-btn mt-30" data-animation="fadeInUp" data-delay="700ms">View Details</a>
                 </div>
