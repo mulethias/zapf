d1006fe64d2bd67bfda032dbed910379b3844019
Rabson <rabsoninnora@gmail.com>
2019-05-13 17:32:22 +0200

    updated front carousel area

--- wp-content/themes/innora/projects-area.php
+++ wp-content/themes/innora/projects-area.php
@@ -1,107 +1,93 @@
 <!-- Projects Area Start -->
 <section class="roberto-project-area">
 
-<?php  $room = get_field('events');?>
+<?php  $room = get_field('carousel');?>
         <!-- Projects Slide -->
         <div class="projects-slides owl-carousel">
             <!-- Single Project Slide -->
-            <div class="single-project-slide active bg-img" style="background-image: url(<?php bloginfo('template_url');?>/img/bg-img/5.jpg);">
+            <div class="single-project-slide active bg-img" style="background-image: url(<?php echo $room['carousel_img_one']?>)">
  
                 <!-- Project Text -->
                 <div class="project-content">
                     <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
+                    <?php echo $room['carousel_heading_one']?>
+   
                     </div>
                 </div>
                 <!-- Hover Effects -->
                 <div class="hover-effects">
                     <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
-                        <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
+                    <?php echo $room['carousel_message_one']?>
                     </div>
-                    <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    <a href="#" class="btn project-btn"><?php echo $room['carousel_button']?><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
                 </div>
             </div>
 
             <!-- Single Project Slide -->
-            <div class="single-project-slide bg-img" style="background-image: url(<?php bloginfo('template_url');?>/img/bg-img/6.jpg);">
+            <div class="single-project-slide bg-img" style="background-image: url(<?php echo $room['carousel_img_two']?>)">
                 <!-- Project Text -->
                 <div class="project-content">
                     <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
+                    <?php echo $room['carousel_heading_two']?>
                     </div>
                 </div>
                 <!-- Hover Effects -->
                 <div class="hover-effects">
                     <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
-                        <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
+                    <?php echo $room['carousel_message_two']?>
                     </div>
-                    <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    <a href="#" class="btn project-btn"><?php echo $room['carousel_button']?><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
                 </div>
             </div>
 
             <!-- Single Project Slide -->
-            <div class="single-project-slide bg-img" style="background-image: url(<?php bloginfo('template_url');?>/img/bg-img/7.jpg);">
+            <div class="single-project-slide bg-img" style="background-image: url(<?php echo $room['carousel_img_three']?>)">
                 <!-- Project Text -->
                 <div class="project-content">
                     <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
+                    <?php echo $room['carousel_heading_three']?>
                     </div>
                 </div>
                 <!-- Hover Effects -->
                 <div class="hover-effects">
                     <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
-                        <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
+                    <?php echo $room['carousel_message_three']?>
                     </div>
-                    <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    <a href="#" class="btn project-btn"><?php echo $room['carousel_button']?> <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
                 </div>
             </div>
 
             <!-- Single Project Slide -->
-            <div class="single-project-slide bg-img" style="background-image: url(<?php bloginfo('template_url');?>/img/bg-img/8.jpg);">
+            <div class="single-project-slide bg-img" style="background-image: url(<?php echo $room['carousel_img_four']?>)">
                 <!-- Project Text -->
                 <div class="project-content">
                     <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
+                    <?php echo $room['carousel_heading_four']?>
                     </div>
                 </div>
                 <!-- Hover Effects -->
                 <div class="hover-effects">
-                    <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
-                        <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
+                    <div class="text"> 
+                    <?php echo $room['carousel_message_four']?>
                     </div>
-                    <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    <a href="#" class="btn project-btn"><?php echo $room['carousel_button']?><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
                 </div>
             </div>
 
             <!-- Single Project Slide -->
-            <div class="single-project-slide bg-img" style="background-image: url(<?php bloginfo('template_url');?>/img/bg-img/9.jpg);">
+            <div class="single-project-slide bg-img" style="background-image: url(<?php echo $room['carousel_img_five']?>)">
                 <!-- Project Text -->
                 <div class="project-content">
                     <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
+                    <?php echo $room['carousel_heading_five']?>
                     </div>
                 </div>
                 <!-- Hover Effects -->
                 <div class="hover-effects">
                     <div class="text">
-                        <h6>Entertaiment</h6>
-                        <h5>Racing Bike</h5>
-                        <p>I focus a lot on helping the first time or inexperienced traveler head out prepared and confident...</p>
+                    <?php echo $room['carousel_message_five']?>
                     </div>
-                    <a href="#" class="btn project-btn">Discover Now <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    <a href="#" class="btn project-btn"><?php echo $room['carousel_button']?><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
                 </div>
             </div>
         </div>
