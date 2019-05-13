4150eaae0bd7c920eb95b4f38e51d67e64c69bf4
Rabson <rabsoninnora@gmail.com>
2019-05-13 15:18:08 +0200

    updated front contact area

--- wp-content/themes/innora/front-page.php
+++ wp-content/themes/innora/front-page.php
@@ -437,16 +437,17 @@ include_once('sliders.php')
     <!-- Call To Action Area Start -->
     <section class="roberto-cta-area">
         <div class="container">
-            <div class="cta-content bg-img bg-overlay jarallax" style="background-image: url(<?php bloginfo('template_url');?>/img/bg-img/1.jpg);">
+        <?php  $contact = get_field('about_images_front');?>
+            <div class="cta-content bg-img bg-overlay jarallax" style="background-image: url(<?php echo $contact['contact_us_image']?>)">
                 <div class="row align-items-center">
                     <div class="col-12 col-md-7">
-                        <div class="cta-text mb-50">
-                            <h2>Contact us now!</h2>
-                            <h6>Contact 1-214 399 0398 to book directly or for advice</h6>
+                        <div class="cta-text mb-50 text-color white">
+                        <?php echo $contact['contact_us_message']?>
+                            
                         </div>
                     </div>
                     <div class="col-12 col-md-5 text-right">
-                        <a href="#" class="btn roberto-btn mb-50">Contact Now</a>
+                        <a href="#" class="btn roberto-btn mb-50"><?php echo $contact['contact_us_button']?></a>
                     </div>
                 </div>
             </div>
